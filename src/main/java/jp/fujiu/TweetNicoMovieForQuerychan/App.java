0fe45aa4c9506bcb36177977713e09d48ddd6043
Mtk Fujiu.jp <mtk-f@live.jp>
2016-03-17 17:52:57 +0900

    検索結果の全件数をツイート内容に追加した

--- src/main/java/jp/fujiu/TweetNicoMovieForQuerychan/App.java
+++ src/main/java/jp/fujiu/TweetNicoMovieForQuerychan/App.java
@@ -82,8 +82,12 @@ public class App {
 		String rawJson;
 		Client client = Client.create();
 		WebResource webResource = client.resource(URL);
-		rawJson = webResource.type(MediaType.APPLICATION_JSON_TYPE).post(String.class, postContent);
+		rawJson = webResource.type(MediaType.APPLICATION_JSON_TYPE)
+				.post(String.class, postContent);
 		client.getExecutorService().shutdown();
+		
+		// 1行目のJSONを抜き出す.
+		String rawJson1stLine = rawJson.substring(0, rawJson.indexOf("\n", 1));
 		
 		// 2行目のJSONを抜き出す.
 		int cr2ndPosition = -1;
@@ -101,7 +105,8 @@ public class App {
 		int cr3drPosition = rawJson.indexOf("\n", cr2ndPosition);
 		if (cr3drPosition == -1) {
 			System.out.println(rawJson);
-			String msg = String.format("%1$s %2$s %3$s", TWITTER_SCREEN_NAME, "JSON \\n error", TWEET_TAG);
+			String msg = String.format("%1$s %2$s %3$s", 
+					TWITTER_SCREEN_NAME, "JSON \\n error", TWEET_TAG);
 			twitter.updateStatus(msg);
 			return;
 		}
@@ -111,7 +116,19 @@ public class App {
 		// 検索結果のJSONをパースする.
 		ObjectMapper mapper = new ObjectMapper();
 		try {
-			// JSONの2行目をパースする.
+			// JSONの1行目をデシリアイズする.
+			Json1stDefinition json1st = mapper.readValue(rawJson1stLine, Json1stDefinition.class);
+			if (json1st.values == null) {
+				String msg = String.format("%1$s %2$s %3$s", 
+						TWITTER_SCREEN_NAME, "response.json1st.values was null",
+						TWEET_TAG);
+				if (IS_TWEETABLE) {
+					twitter.updateStatus(msg);
+				}
+				return;
+			}
+			
+			// JSONの2行目をデシリアイズする.
 			Json2ndDefinition json2nd = mapper.readValue(rawJson2ndLine, Json2ndDefinition.class);
 			
 			if (json2nd.values == null) {
@@ -125,33 +142,34 @@ public class App {
 			}
 			
 			// 動画をランダムに決める.
-			int total = json2nd.values.length;
+			int recentTotal = json2nd.values.length;
 			Random random = new Random();
-			int index = random.nextInt(total);
+			int index = random.nextInt(recentTotal);
 			
 			// 動画を選ぶ.
 			Json2ndDefinitionValue value = json2nd.values[index];
 
-			// 長すぎるタイトルを省略する.
+			// タイトルが長すぎれば省略する.
 			final int maxLength = 40;
 			String title = Normalizer.normalize(value.title, Normalizer.Form.NFC).length() 
 					< maxLength ? value.title : value.title.substring(0, maxLength) + "...";
 
 			String msg = String.format(
-					"今日の %1$s のおすすめ動画は･･･\n" 
+					"今日の %1$s のおすすめ動画\n" 
 					+ "%2$s\n"
 					+ "http://www.nicovideo.jp/watch/%3$s\n" 
-					+ "最新%4$d件から %5$s", 
+					+ "全%4$d件中最新の%5$d件から %6$s", 
 					QUERY,
 					StringEscapeUtils.unescapeHtml4(title), 
 					value.cmsid,
-					total, 
+					json1st.values[0].total,
+					recentTotal, 
 					TWEET_TAG);
 
 			System.out.println(msg);
 			
 			// 選んだ動画をツイッターに投稿する.
-			// ただし環境変数 ApplicationEnabled が true のときのみ.
+			// ただし環境変数 が 許可しているのときのみ.
 			if (IS_TWEETABLE) {
 				twitter.updateStatus(msg);
 			}
